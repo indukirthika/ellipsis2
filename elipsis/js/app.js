eaed8dc20e95ffd2863b427d1faad21827cde2dd
Indu <indhukirthika@gmail.com>
2016-06-08 15:24:48 +0530

    Add files via upload

---
+++ elipsis/js/app.js
@@ -0,0 +1,32 @@
+new WOW().init();
+
+function collapseNavbar() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+        $('.logo1').show();
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $('.logo1').hide();
+    }
+}
+
+$(window).scroll(collapseNavbar);
+$(document).ready(collapseNavbar);
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+  if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
+    $('.navbar-toggle:visible').click();
+  }
+});
